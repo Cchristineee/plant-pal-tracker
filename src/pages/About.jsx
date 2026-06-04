e7bfb3f0c77c5bef0db3603c94edf83c66dafe21
Christine Grimadeau <cgrimad1@yahoo.com>
2026-06-04 19:23:46 -0400

    Added components for the About, Dashboard and Home sections

---
+++ src/pages/About.jsx
@@ -0,0 +1,15 @@
+function About () {
+    return (
+        <div>
+            <h1>About Plant Pal</h1>
+            <h3>Life is Better with Plants!</h3>
+
+            <p>
+                Plant Pal helps plant owners track watering schedules, sunlight requirements, 
+                and plant care routines.
+            </p>
+        </div>
+    );
+}
+
+export default About;
