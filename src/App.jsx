72703726c0e1e705909994b2e77c324b365393a6
Christine Grimadeau <cgrimad1@yahoo.com>
2026-06-04 20:24:20 -0400

    Added route paths for Home, About, & Dashboard tabs

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,40 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
 import "./App.css";
 
-/* Page Conponents */
-import About from "./pages/About";
-
-
+function Home () {
+  return (
+    <section className="hero">
+      <div className="hero-content">
+        <div className="hero-logo">🪴</div>
+        <h1>Keep Your Plants Healthy and Happy!</h1>
+        <p>
+        Track watering, light needs, and care routines for all your plants.
+        </p>
+        <button className="hero-button">Add Your First Plant</button>
+      </div>
+    </section>
+  )
+}
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      
-      <section className="hero">
-        <div className="hero-content">
-        <div className="hero-logo">🪴</div>
-        <h1>Keep Your Plants Healthy and Happy!</h1>
-        <p>Track watering, light needs, and care routines for all your plants.</p>
-        <button className="hero-button">Add Your First Plant</button>
-        </div>
-      </section>
-      
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+
       <Footer />
-    </>
+    </Router>
   );
 }
 
