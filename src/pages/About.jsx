d300d7e715d1068a3e496494ffd5dfe6efec8602
Christine Grimadeau <cgrimad1@yahoo.com>
2026-06-04 20:35:22 -0400

    Created About section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,20 @@
-function About () {
-    return (
-        <div>
-            <h1>About Plant Pal</h1>
-            <h3>Life is Better with Plants!</h3>
+import React from 'react';
+import "./About.css";
 
-            <p>
-                Plant Pal helps plant owners track watering schedules, sunlight requirements, 
-                and plant care routines.
-            </p>
-        </div>
-    );
+function About() {
+  return (
+    <section className="about-container">
+      <h1>About Plant Pal</h1>
+
+      <h3>Life is Better with Plants! 🌱</h3>
+
+       <p>
+        Plant Pal helps plant owners track watering schedules, sunlight
+        requirements, and care routines so their plants stay healthy and
+        thriving.
+      </p>
+    </section>
+  );
 }
 
 export default About;
